d7882bf70019a268da01e0aec016a72cdde8897b
mena afefe fawze <73434035+MNaguib2@users.noreply.github.com>
2022-04-20 20:48:41 +0200

    Handle script cash

--- src/SharpFunction.ts
+++ src/SharpFunction.ts
@@ -1,11 +1,17 @@
 import sharp from "sharp";
+import fs from "fs";
 async function ProcessImage(fullname: string, width: number, height: number) {
     const Newfullname: string = `filename=${fullname.split('.')[0]}&width=${width}&height=${height}.${fullname.split('.').pop()}`;
-    await sharp(`./assets/full/${fullname}`)
+    
+    try{
+        const file = fs.readFileSync(process.cwd()+'/assets/thumbnail/'+Newfullname);
+    }catch(error) {
+        await sharp(`./assets/full/${fullname}`)
         .resize(+width, +height)
         .toFile('./assets/thumbnail/' + Newfullname).then(result => {
-            //console.log('Image Convert Successful!!');
+            console.log('Image Convert Successful!!');
         })
+    }  
     return Newfullname;
 }
 
